2c89cf49f14a600c547f9afde224483ba5a41454
Thomas Landais <thomas.landais9733@gmail.com>
2021-10-24 15:09:05 +0200

    ajout delete modify create pour la route post

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const db = require("./config/db");
 const app = express();
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
+const path =  require("path");
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -14,5 +15,7 @@ app.use((req, res, next) => {
 
 app.use('/api/user',userRoutes);
 app.use('/api/post',postRoutes);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -6,5 +6,7 @@ const multer = require("../middleware/multer");
 
 router.get("/", postCtrl.getAll);
 router.post("/", multer, postCtrl.create);
+router.delete("/:id", postCtrl.delete);
+router.put("/:id", multer, postCtrl.modify);
 
 module.exports = router;
